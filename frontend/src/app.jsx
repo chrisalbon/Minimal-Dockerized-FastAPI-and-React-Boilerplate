6e5cac0deee57d6c4c05d9be8305adbc2fdfdf87
chrisalbon <chris.albon@gmail.com>
2024-09-07 15:30:03 -0700

    grammar nit

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -12,7 +12,7 @@ function App() {
 
     return (
         <div className="App">
-            <p>This message is coming from the React frontend</p>
+            <p>This message is coming from the React frontend.</p>
             <p>{message}</p>
         </div>
     )
